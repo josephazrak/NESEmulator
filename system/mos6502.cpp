1cfe42e7fd3590140d41092ce1da8458411a9531
Joseph Azrak <azrakjo@gmail.com>
2021-02-08 11:50:21 +0300

    begin implementing addressing modes & operations

--- system/mos6502.cpp
+++ system/mos6502.cpp
@@ -12,8 +12,31 @@
 #include <iomanip>
 #include <bitset>
 
-MOS6502::MOS6502(RAM& ram) {
-    NES_Ram = ram;
+MOS6502::MOS6502(RAM& ram_ref) {
+    // Bind this CPU to RAM
+    NES_Ram = ram_ref;
+
+    // Create the lookup table. Thank you javidx9 (https://github.com/OneLoneCoder) for the hard work compiling this!
+    using a = MOS6502;
+    lookup_table =
+            {
+                    { "BRK", &a::BRK, &a::IMM, 7 },{ "ORA", &a::ORA, &a::IZX, 6 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 3 },{ "ORA", &a::ORA, &a::ZP0, 3 },{ "ASL", &a::ASL, &a::ZP0, 5 },{ "???", &a::XXX, &a::IMP, 5 },{ "PHP", &a::PHP, &a::IMP, 3 },{ "ORA", &a::ORA, &a::IMM, 2 },{ "ASL", &a::ASL, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::NOP, &a::IMP, 4 },{ "ORA", &a::ORA, &a::ABS, 4 },{ "ASL", &a::ASL, &a::ABS, 6 },{ "???", &a::XXX, &a::IMP, 6 },
+                    { "BPL", &a::BPL, &a::REL, 2 },{ "ORA", &a::ORA, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 4 },{ "ORA", &a::ORA, &a::ZPX, 4 },{ "ASL", &a::ASL, &a::ZPX, 6 },{ "???", &a::XXX, &a::IMP, 6 },{ "CLC", &a::CLC, &a::IMP, 2 },{ "ORA", &a::ORA, &a::ABY, 4 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 7 },{ "???", &a::NOP, &a::IMP, 4 },{ "ORA", &a::ORA, &a::ABX, 4 },{ "ASL", &a::ASL, &a::ABX, 7 },{ "???", &a::XXX, &a::IMP, 7 },
+                    { "JSR", &a::JSR, &a::ABS, 6 },{ "AND", &a::AND, &a::IZX, 6 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "BIT", &a::BIT, &a::ZP0, 3 },{ "AND", &a::AND, &a::ZP0, 3 },{ "ROL", &a::ROL, &a::ZP0, 5 },{ "???", &a::XXX, &a::IMP, 5 },{ "PLP", &a::PLP, &a::IMP, 4 },{ "AND", &a::AND, &a::IMM, 2 },{ "ROL", &a::ROL, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 2 },{ "BIT", &a::BIT, &a::ABS, 4 },{ "AND", &a::AND, &a::ABS, 4 },{ "ROL", &a::ROL, &a::ABS, 6 },{ "???", &a::XXX, &a::IMP, 6 },
+                    { "BMI", &a::BMI, &a::REL, 2 },{ "AND", &a::AND, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 4 },{ "AND", &a::AND, &a::ZPX, 4 },{ "ROL", &a::ROL, &a::ZPX, 6 },{ "???", &a::XXX, &a::IMP, 6 },{ "SEC", &a::SEC, &a::IMP, 2 },{ "AND", &a::AND, &a::ABY, 4 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 7 },{ "???", &a::NOP, &a::IMP, 4 },{ "AND", &a::AND, &a::ABX, 4 },{ "ROL", &a::ROL, &a::ABX, 7 },{ "???", &a::XXX, &a::IMP, 7 },
+                    { "RTI", &a::RTI, &a::IMP, 6 },{ "EOR", &a::EOR, &a::IZX, 6 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 3 },{ "EOR", &a::EOR, &a::ZP0, 3 },{ "LSR", &a::LSR, &a::ZP0, 5 },{ "???", &a::XXX, &a::IMP, 5 },{ "PHA", &a::PHA, &a::IMP, 3 },{ "EOR", &a::EOR, &a::IMM, 2 },{ "LSR", &a::LSR, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 2 },{ "JMP", &a::JMP, &a::ABS, 3 },{ "EOR", &a::EOR, &a::ABS, 4 },{ "LSR", &a::LSR, &a::ABS, 6 },{ "???", &a::XXX, &a::IMP, 6 },
+                    { "BVC", &a::BVC, &a::REL, 2 },{ "EOR", &a::EOR, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 4 },{ "EOR", &a::EOR, &a::ZPX, 4 },{ "LSR", &a::LSR, &a::ZPX, 6 },{ "???", &a::XXX, &a::IMP, 6 },{ "CLI", &a::CLI, &a::IMP, 2 },{ "EOR", &a::EOR, &a::ABY, 4 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 7 },{ "???", &a::NOP, &a::IMP, 4 },{ "EOR", &a::EOR, &a::ABX, 4 },{ "LSR", &a::LSR, &a::ABX, 7 },{ "???", &a::XXX, &a::IMP, 7 },
+                    { "RTS", &a::RTS, &a::IMP, 6 },{ "ADC", &a::ADC, &a::IZX, 6 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 3 },{ "ADC", &a::ADC, &a::ZP0, 3 },{ "ROR", &a::ROR, &a::ZP0, 5 },{ "???", &a::XXX, &a::IMP, 5 },{ "PLA", &a::PLA, &a::IMP, 4 },{ "ADC", &a::ADC, &a::IMM, 2 },{ "ROR", &a::ROR, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 2 },{ "JMP", &a::JMP, &a::IND, 5 },{ "ADC", &a::ADC, &a::ABS, 4 },{ "ROR", &a::ROR, &a::ABS, 6 },{ "???", &a::XXX, &a::IMP, 6 },
+                    { "BVS", &a::BVS, &a::REL, 2 },{ "ADC", &a::ADC, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 4 },{ "ADC", &a::ADC, &a::ZPX, 4 },{ "ROR", &a::ROR, &a::ZPX, 6 },{ "???", &a::XXX, &a::IMP, 6 },{ "SEI", &a::SEI, &a::IMP, 2 },{ "ADC", &a::ADC, &a::ABY, 4 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 7 },{ "???", &a::NOP, &a::IMP, 4 },{ "ADC", &a::ADC, &a::ABX, 4 },{ "ROR", &a::ROR, &a::ABX, 7 },{ "???", &a::XXX, &a::IMP, 7 },
+                    { "???", &a::NOP, &a::IMP, 2 },{ "STA", &a::STA, &a::IZX, 6 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 6 },{ "STY", &a::STY, &a::ZP0, 3 },{ "STA", &a::STA, &a::ZP0, 3 },{ "STX", &a::STX, &a::ZP0, 3 },{ "???", &a::XXX, &a::IMP, 3 },{ "DEY", &a::DEY, &a::IMP, 2 },{ "???", &a::NOP, &a::IMP, 2 },{ "TXA", &a::TXA, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 2 },{ "STY", &a::STY, &a::ABS, 4 },{ "STA", &a::STA, &a::ABS, 4 },{ "STX", &a::STX, &a::ABS, 4 },{ "???", &a::XXX, &a::IMP, 4 },
+                    { "BCC", &a::BCC, &a::REL, 2 },{ "STA", &a::STA, &a::IZY, 6 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 6 },{ "STY", &a::STY, &a::ZPX, 4 },{ "STA", &a::STA, &a::ZPX, 4 },{ "STX", &a::STX, &a::ZPY, 4 },{ "???", &a::XXX, &a::IMP, 4 },{ "TYA", &a::TYA, &a::IMP, 2 },{ "STA", &a::STA, &a::ABY, 5 },{ "TXS", &a::TXS, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 5 },{ "???", &a::NOP, &a::IMP, 5 },{ "STA", &a::STA, &a::ABX, 5 },{ "???", &a::XXX, &a::IMP, 5 },{ "???", &a::XXX, &a::IMP, 5 },
+                    { "LDY", &a::LDY, &a::IMM, 2 },{ "LDA", &a::LDA, &a::IZX, 6 },{ "LDX", &a::LDX, &a::IMM, 2 },{ "???", &a::XXX, &a::IMP, 6 },{ "LDY", &a::LDY, &a::ZP0, 3 },{ "LDA", &a::LDA, &a::ZP0, 3 },{ "LDX", &a::LDX, &a::ZP0, 3 },{ "???", &a::XXX, &a::IMP, 3 },{ "TAY", &a::TAY, &a::IMP, 2 },{ "LDA", &a::LDA, &a::IMM, 2 },{ "TAX", &a::TAX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 2 },{ "LDY", &a::LDY, &a::ABS, 4 },{ "LDA", &a::LDA, &a::ABS, 4 },{ "LDX", &a::LDX, &a::ABS, 4 },{ "???", &a::XXX, &a::IMP, 4 },
+                    { "BCS", &a::BCS, &a::REL, 2 },{ "LDA", &a::LDA, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 5 },{ "LDY", &a::LDY, &a::ZPX, 4 },{ "LDA", &a::LDA, &a::ZPX, 4 },{ "LDX", &a::LDX, &a::ZPY, 4 },{ "???", &a::XXX, &a::IMP, 4 },{ "CLV", &a::CLV, &a::IMP, 2 },{ "LDA", &a::LDA, &a::ABY, 4 },{ "TSX", &a::TSX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 4 },{ "LDY", &a::LDY, &a::ABX, 4 },{ "LDA", &a::LDA, &a::ABX, 4 },{ "LDX", &a::LDX, &a::ABY, 4 },{ "???", &a::XXX, &a::IMP, 4 },
+                    { "CPY", &a::CPY, &a::IMM, 2 },{ "CMP", &a::CMP, &a::IZX, 6 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "CPY", &a::CPY, &a::ZP0, 3 },{ "CMP", &a::CMP, &a::ZP0, 3 },{ "DEC", &a::DEC, &a::ZP0, 5 },{ "???", &a::XXX, &a::IMP, 5 },{ "INY", &a::INY, &a::IMP, 2 },{ "CMP", &a::CMP, &a::IMM, 2 },{ "DEX", &a::DEX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 2 },{ "CPY", &a::CPY, &a::ABS, 4 },{ "CMP", &a::CMP, &a::ABS, 4 },{ "DEC", &a::DEC, &a::ABS, 6 },{ "???", &a::XXX, &a::IMP, 6 },
+                    { "BNE", &a::BNE, &a::REL, 2 },{ "CMP", &a::CMP, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 4 },{ "CMP", &a::CMP, &a::ZPX, 4 },{ "DEC", &a::DEC, &a::ZPX, 6 },{ "???", &a::XXX, &a::IMP, 6 },{ "CLD", &a::CLD, &a::IMP, 2 },{ "CMP", &a::CMP, &a::ABY, 4 },{ "NOP", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 7 },{ "???", &a::NOP, &a::IMP, 4 },{ "CMP", &a::CMP, &a::ABX, 4 },{ "DEC", &a::DEC, &a::ABX, 7 },{ "???", &a::XXX, &a::IMP, 7 },
+                    { "CPX", &a::CPX, &a::IMM, 2 },{ "SBC", &a::SBC, &a::IZX, 6 },{ "???", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "CPX", &a::CPX, &a::ZP0, 3 },{ "SBC", &a::SBC, &a::ZP0, 3 },{ "INC", &a::INC, &a::ZP0, 5 },{ "???", &a::XXX, &a::IMP, 5 },{ "INX", &a::INX, &a::IMP, 2 },{ "SBC", &a::SBC, &a::IMM, 2 },{ "NOP", &a::NOP, &a::IMP, 2 },{ "???", &a::SBC, &a::IMP, 2 },{ "CPX", &a::CPX, &a::ABS, 4 },{ "SBC", &a::SBC, &a::ABS, 4 },{ "INC", &a::INC, &a::ABS, 6 },{ "???", &a::XXX, &a::IMP, 6 },
+                    { "BEQ", &a::BEQ, &a::REL, 2 },{ "SBC", &a::SBC, &a::IZY, 5 },{ "???", &a::XXX, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 8 },{ "???", &a::NOP, &a::IMP, 4 },{ "SBC", &a::SBC, &a::ZPX, 4 },{ "INC", &a::INC, &a::ZPX, 6 },{ "???", &a::XXX, &a::IMP, 6 },{ "SED", &a::SED, &a::IMP, 2 },{ "SBC", &a::SBC, &a::ABY, 4 },{ "NOP", &a::NOP, &a::IMP, 2 },{ "???", &a::XXX, &a::IMP, 7 },{ "???", &a::NOP, &a::IMP, 4 },{ "SBC", &a::SBC, &a::ABX, 4 },{ "INC", &a::INC, &a::ABX, 7 },{ "???", &a::XXX, &a::IMP, 7 },
+            };
 }
 
 MOS6502::~MOS6502() = default;
@@ -33,12 +56,12 @@ void MOS6502::flip_flag(flag_t flag)
     FLG = FLG ^ flag.bitmask;
 }
 
-byte_t MOS6502::get_flag(flag_t flag) const
+uint8_t MOS6502::get_flag(flag_t flag) const
 {
     return (FLG & flag.bitmask) >> flag.bitshift;
 }
 
-byte_t MOS6502::get_status() const
+uint8_t MOS6502::get_status() const
 {
     return FLG;
 }
@@ -49,7 +72,7 @@ void MOS6502::whoami() const
     std::cout << "MOS6502 EMULATED INSTANCE (status=" << status << ")" << std::endl;
     std::cout << "---REGISTERS---" << std::endl;
     std::cout << "ACC=0d" << std::setw(4) << ACC << ";      RAX=0d" << std::setw(4) << X << "; RAY=0d" << std::setw(4) << Y << std::endl;
-    std::cout << "FLG=0b" << std::bitset<8> (FLG) << "; PC=0x" << std::hex << std::setw(5) << PC << "; SP=0x" << std::setw(5) << SP << std::dec << std::endl;
+    std::cout << "FLG=0b" << std::bitset<8> (FLG) << "; PC=$" << std::hex << std::setw(5) << PC << "; SP=$" << std::setw(5) << SP << std::dec << std::endl;
     std::cout << "---MISC---" << std::endl;
     std::cout << "FETCH_DATA: " << fetched << std::endl;
     std::cout << "---FLAGS--- " << std::endl;
@@ -57,3 +80,134 @@ void MOS6502::whoami() const
     std::cout << "NVssDIZC" << std::endl;
     std::cout << "===================================" << std::endl;
 }
+
+void MOS6502::reset()
+{
+    // Reset registers
+    ACC, X, Y, FLG, PC, SP = 0;
+
+    // Reset pseudo-registers
+    fetched = 0;
+}
+
+void MOS6502::fetch_data()
+{
+    fetched = NES_Ram.read_byte(fetch_address);
+}
+
+// ===========================
+// ADDRESSING MODES
+// ===========================
+
+// An addressing mode function retrieves data as implemented by that addressing mode.
+// It stores the data fetched in the `fetched' variable, which is then used by the operation
+// function. It returns an integer representing the additional number of cycles necessary
+// to emulate to faithfully reproduce the 6502's behavior.
+
+/**
+ * Addressing mode: implied.
+ */
+uint8_t MOS6502::IMP()
+{
+    fetched = ACC;
+    return 0;
+}
+
+/**
+ * Addressing mode: immediate.
+ */
+uint8_t MOS6502::IMM()
+{
+    // In this addressing mode, the address to fetch is usually immediately after the
+    // instruction (located @ PC). As such, increment PC and fetch the byte at PC.
+
+    PC += 1;
+    fetched = NES_Ram.read_byte(PC);
+
+    return 0;
+}
+
+/**
+ * Addressing mode: zero-page.
+ */
+uint8_t MOS6502::ZP0()
+{
+
+}
+
+/**
+ * Addressing mode: zero-page, x-offset.
+ */
+uint8_t MOS6502::ZPX()
+{
+
+}
+
+/**
+ * Addressing mode: zero-page, y-offset.
+ */
+uint8_t MOS6502::ZPY()
+{
+
+}
+
+/**
+ * Addressing mode: relative.
+ */
+uint8_t MOS6502::REL()
+{
+
+}
+
+/**
+ * Addressing mode: absolute.
+ */
+uint8_t MOS6502::ABS()
+{
+
+}
+
+/**
+ * Addressing mode: absolute, x-offset.
+ */
+uint8_t MOS6502::ABX()
+{
+
+}
+
+/**
+ * Addressing mode: absolute, y-offset.
+ */
+uint8_t MOS6502::ABY()
+{
+
+}
+
+/**
+ * Addressing mode: indirect.
+ */
+uint8_t MOS6502::IND()
+{
+
+}
+
+/**
+ * Addressing mode: indirect, x-offset.
+ */
+uint8_t MOS6502::IZX()
+{
+
+}
+
+/**
+ * Addressing mode: indirect, y-offset.
+ */
+uint8_t MOS6502::IZY()
+{
+
+}
+
+// ===========================
+// OPERATIONS
+// ===========================
+
--- system/mos6502.hpp
+++ system/mos6502.hpp
@@ -9,19 +9,18 @@
 #include <vector>
 #include <string>
 
-typedef uint8_t byte_t;
-
 struct opcode
 {
-    byte_t opcode_byte;   // A single byte which identifies this operation
+    uint8_t opcode_byte;   // A single byte which identifies this operation
 };
 
 struct flag_t
 {
-    byte_t bitmask;
-    byte_t bitshift;
+    uint8_t bitmask;
+    uint8_t bitshift;
 };
 
+struct instruction_t; // forward-declaration
 
 // Bitmasks for the 6502 microprocessor's status register.
 const flag_t FLAG_C_CARRY = {1 << 0, 0};
@@ -39,24 +38,57 @@ private:
     // CPU status human-readable string
     std::string status = "stopped";
 
-    // Pointer to the RAM object.
+    // Pointer to the RAM object. RAM also contains ROM. Ha!
     RAM NES_Ram;
 
     // CPU registers.
-    byte_t ACC = 0b00000000; // Accumulator.
-    byte_t X = 0b00000000;   // X-register.
-    byte_t Y = 0b00000000;   // Y-register.
-    byte_t FLG = 0b00000000; // Status flags.
-    byte_t PC = 0b00000000;  // Program counter.
-    byte_t SP = 0b00000000;  // Stack pointer.
+    uint8_t ACC = 0b00000000; // Accumulator.
+    uint8_t X = 0b00000000;   // X-register.
+    uint8_t Y = 0b00000000;   // Y-register.
+    uint8_t FLG = 0b00000000; // Status flags.
+    uint8_t PC = 0b00000000;  // Program counter.
+    uint8_t SP = 0b00000000;  // Stack pointer.
 
     // Other intermediate data.
-    byte_t fetched = 0b0000000; // Fetched data.
+    uint8_t fetched = 0b0000000; // Fetched data.
+    address_t fetch_address = 0x0000; // Next fetch address (set by addressing mode function).
 
+    // Fetch function.
+    void fetch_data();
 
     // Opcode table.
+    std::vector<instruction_t> lookup_table;
+
+    // Opcode definitions. Thank you javidx9! These should return
+    // 0 in most cases and return 1 when an additional clock cycle
+    // is needed.
+
+    uint8_t ADC();	uint8_t AND();	uint8_t ASL();	uint8_t BCC();
+    uint8_t BCS();	uint8_t BEQ();	uint8_t BIT();	uint8_t BMI();
+    uint8_t BNE();	uint8_t BPL();	uint8_t BRK();	uint8_t BVC();
+    uint8_t BVS();	uint8_t CLC();	uint8_t CLD();	uint8_t CLI();
+    uint8_t CLV();	uint8_t CMP();	uint8_t CPX();	uint8_t CPY();
+    uint8_t DEC();	uint8_t DEX();	uint8_t DEY();	uint8_t EOR();
+    uint8_t INC();	uint8_t INX();	uint8_t INY();	uint8_t JMP();
+    uint8_t JSR();	uint8_t LDA();	uint8_t LDX();	uint8_t LDY();
+    uint8_t LSR();	uint8_t NOP();	uint8_t ORA();	uint8_t PHA();
+    uint8_t PHP();	uint8_t PLA();	uint8_t PLP();	uint8_t ROL();
+    uint8_t ROR();	uint8_t RTI();	uint8_t RTS();	uint8_t SBC();
+    uint8_t SEC();	uint8_t SED();	uint8_t SEI();	uint8_t STA();
+    uint8_t STX();	uint8_t STY();	uint8_t TAX();	uint8_t TAY();
+    uint8_t TSX();	uint8_t TXA();	uint8_t TXS();	uint8_t TYA();
+
+    uint8_t XXX(); // Functionally a nop
+
+    // Addressing modes. Thank you javidx9! These functions return the
+    // adjustment needed in the clock cycles (additional clock cycles
+    // that may be needed).
+
+    uint8_t IMP(); uint8_t IMM(); uint8_t ZP0(); uint8_t ZPX();
+    uint8_t ZPY(); uint8_t REL(); uint8_t ABS(); uint8_t ABX();
+    uint8_t ABY(); uint8_t IND(); uint8_t IZX(); uint8_t IZY();
 public:
-    explicit MOS6502(RAM& ram_pointer);
+    explicit MOS6502(RAM& ram_ref);
     ~MOS6502();
 
     // Sets a single flag (sets it to 1).
@@ -69,20 +101,32 @@ public:
     void flip_flag(flag_t flag);
 
     // Returns the value of a single flag.
-    byte_t get_flag(flag_t flag) const;
+    uint8_t get_flag(flag_t flag) const;
 
-    // Get all the flags in a byte.
-    byte_t get_status() const;
+    // Get all the flags.
+    uint8_t get_status() const;
 
     // Print human-readable processor status.
     void whoami() const;
+
+    // Reset the processor.
+    void reset();
 };
 
-struct instruction_t { // In the 6502, each instruction has an ADDRESSING MODE, an OPERATION, a BYTES value, and a CYCLES value.
-    std::string name; // Human-readable name of the instruction (mnemonic).
-    byte_t (MOS6502::*operate)(void) = nullptr; // Operation function for this instruction.
-    byte_t (MOS6502::*addrmode)(void) = nullptr; // Addressing mode for this instruction.
-    byte_t cycles = 0; // Number of clock cycles this instruction should 'fake'.
+/**
+ * In the 6502, each instruction has an ADDRESSING MODE, an OPERATION, a BYTES value, and a CYCLES value.
+ * In the INSTRUCTION LOOKUP TABLE, the relative position of each instruction_t struct is the actual opcode.
+ * As such, there should be 256 values in the INSTRUCTION LOOKUP TABLE.
+ *
+ * The INSTRUCTION LOOKUP TABLE is populated when the CPU object is instantiated.
+ *
+ * Thank you javidx9 (OneLoneCoder) for the instruction lookup table (https://github.com/OneLoneCoder).
+ */
+struct instruction_t {
+    std::string name;                               // Human-readable name of the instruction (mnemonic).
+    uint8_t (MOS6502::*operate)(void) = nullptr;    // Operation function for this instruction.
+    uint8_t (MOS6502::*addrmode)(void) = nullptr;   // Addressing mode function for this instruction.
+    uint8_t cycles = 0;                             // Number of clock cycles this instruction should 'fake'.
 };
 
 #endif //NESEMULATOR_MOS6502_HPP
